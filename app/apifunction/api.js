230e8c8e68204c9fd14614bc367f29786ecb571c
charankota <charandhillon@gmail.com>
2023-09-19 15:11:36 +0530

    Update api.js

--- app/apifunction/api.js
+++ app/apifunction/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 //const baseURL = "http://localhost:5234/api/Concept/"
-const baseURL = "https://192.168.0.241:45460/api/Concept/"
+const baseURL = "https://192.168.0.241:45464/api/Concept/"
 
 //http://localhost:5234/api/Concept/TestQuestions?testid=474018
 
